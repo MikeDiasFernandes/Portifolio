d16cc20989f4fe94a9fbef1583cdf570380b0c6a
MikeDiasFernandes <88415227+MikeDiasFernandes@users.noreply.github.com>
2023-07-17 13:42:55 -0300

    https://github.com/MikeDiasFernandes/Portifolio

--- _javascript/script.js
+++ _javascript/script.js
@@ -64,3 +64,21 @@ const typed = new Typed('.multiple-text',{
 
 
 
+
+/*==========================================  TYPED JS ========================================= */
+
+
+const text = document.querySelector('');
+text.innerHTML = text.textContent.replace(/\S/g,"<span>$&</span>");
+
+const letters = document.querySelectorAll('span');
+for (let i=0; i<letters.length; i++){
+    letters[i].addEventListener("mouseover", function(){
+        letters[i].classList.add('active')
+    })
+}
+
+
+
+
+
